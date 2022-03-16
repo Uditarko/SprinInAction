88789f816726bf7f5f7a7bfc1301f17e824a8792
uditarko <uditarko.dutta@gmail.com>
2022-03-17 03:20:25 +0530

    Here is the base for the rest of development

---
+++ foundational-spring/taco-cloud-one/src/main/java/my/learning/sia/chp1/tacocloudone/TacoCloudOneApplication.java
@@ -0,0 +1,13 @@
+package my.learning.sia.chp1.tacocloudone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudOneApplication.class, args);
+	}
+
+}
---
+++ foundational-spring/taco-cloud-one/src/test/java/my/learning/sia/chp1/tacocloudone/TacoCloudOneApplicationTests.java
@@ -0,0 +1,13 @@
+package my.learning.sia.chp1.tacocloudone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
